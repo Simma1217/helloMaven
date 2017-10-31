42c61cd5cdfa4946bb4e70bcb7408b44dc53f093
simma-home <479921347@qq.com>
2017-10-31 23:25:24 +0800

    kkkk

--- src/main/java/helloMaven/TEST2.java
+++ src/main/java/helloMaven/TEST2.java
@@ -3,7 +3,7 @@ package helloMaven;
 public class TEST2 {
 
 	public static void main(String[] args) {
-		System.out.println("HAHA1444466611");
+		System.out.println("HAHA1444466744477611");
 	}
 
 }
